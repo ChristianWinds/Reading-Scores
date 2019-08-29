1ffc7438f6e5f61e9ca6f1916e2487819471bfd1
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-08-28 22:48:43 -0400

    A push of another iteration of flesch.cpp.

--- cpp/flesch.cpp
+++ cpp/flesch.cpp
@@ -1,7 +1,20 @@
-#include <>
+#include <cstdlib>
+#include <string>
 
+// * PROGRAMMER NOTE: The parentheses beside main may require text to allow
+// * the command line to be an input source.
 int main()
 {
-	//
+	// Read the filename of the file to be analyzed
+	;
 
+	// Obtain the values needed for the readability index and score
+	// calculations
+	// * PROGRAMMER NOTE: Pass-by reference may be required here to
+	// * update the multiple calculation variable values, as C++
+	// * functions can otherwise return only one value to the functions'
+	// * callers.
+	;
+
+	return 0;
 }
