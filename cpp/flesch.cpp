f71e2d06ac5c22151db0d601d6f8050c92c726fc
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-08-28 22:56:58 -0400

    A push of another iteration of flesch.cpp.

--- cpp/flesch.cpp
+++ cpp/flesch.cpp
@@ -8,6 +8,13 @@ int main()
 	// Read the filename of the file to be analyzed
 	;
 
+	// Preapare variables for the four values to be recorded while reading
+	// the input file
+	int syllables = 0;
+	int totalWords = 0;
+	int sentences = 0;
+	int difficultWords = 0;
+
 	// Obtain the values needed for the readability index and score
 	// calculations
 	// * PROGRAMMER NOTE: Pass-by reference may be required here to
