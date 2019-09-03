a08aad280a599b8e031173199eda105dc42dddca
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-02 21:59:35 -0400

    Replacement of endl with '\n' in analyzed file line printing

--- cpp/flesch.cpp
+++ cpp/flesch.cpp
@@ -60,7 +60,7 @@ int main(int argc, char* argv[])
 		while (getline(analyzedFile, line));
 		{
 			// Print the read line
-			cout << line << endl;
+			cout << line << '\n';
 		}
 		analyzedFile.close();
 	}
