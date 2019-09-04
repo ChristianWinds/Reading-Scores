319b5c413d0ff7ec774915bfe216bfd7d5273e7e
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-04 08:43:25 -0400

    Keyboard line input test

--- cpp/flesch.cpp
+++ cpp/flesch.cpp
@@ -76,6 +76,7 @@ int main(int argc, char* argv[])
 			{
 				// ** TEST
 				cout << "Print read line." << endl;
+				cin >> line >> endl;
 /*				if (testMode)
 				{
 					cout << "getline branch entered" << endl;
