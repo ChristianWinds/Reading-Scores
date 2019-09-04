bd25641ed6adc5b1e6acf942388636be71157f21
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-04 11:33:22 -0400

    obtainValues: while loop indent

--- cpp/flesch.cpp
+++ cpp/flesch.cpp
@@ -143,21 +143,21 @@ void obtainValues(&syllables,
 
 	while ()
 	{
-	// Store the earliest unread file line
-	// * PROGRAMMER'S NOTE: Code line [SUCCESSFUL]	
-	getline(analyzedFile, currentLine);
+		// Store the earliest unread file line
+		// * PROGRAMMER'S NOTE: Code line [SUCCESSFUL]	
+		getline(analyzedFile, currentLine);
 
-	// Track the current index read in the input file
-	int currentIndex = 0;
+		// Track the current index read in the input file
+		int currentIndex = 0;
 
-	// Retrieve the next unread word from the line
-	string word = getWord(currentIndex, line);
+		// Retrieve the next unread word from the line
+		string word = getWord(currentIndex, line);
 
-	// Increment the number of words
-	totalWords++;
+		// Increment the number of words
+		totalWords++;
 
-	// Determine the number of syllables in the retrieved word
-	int syllables = countSyllables(word);
+		// Determine the number of syllables in the retrieved word
+		int syllables = countSyllables(word);
 	}
 	
 }
