f79c5428f6257bdbc3c5b3f4d580dd1fbe25a0f7
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-05 11:49:34 -0400

    New goal added: Refine syllable and word detection

--- cpp/flesch.cpp
+++ cpp/flesch.cpp
@@ -7,7 +7,7 @@
 // * PROGRAMMER'S NOTE: Current Goal (9/4/2019): Count the number of syllables
 // * 	- PROGRAMMER'S NOTE: Subgoal (9/4/2019): Add special word end "e"
 // *	  counting rule to algorithm [RUNNING; UNEVALUATED]
-// * in a text file
+// * PROGRAMMER'S NOTE: Current Goal (9/5/2019): Refine word and syllable detection
 // *	- Error (9/4/2019) - Line index not updating properly [CORRECTED]
 // *	- Error (9/4/2019) - Text read becomes out of range at line end
 // *	  [CORRECTED]
