fa7e9bf8bcdebdad94dd75fa6a31e30281b05560
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-11 10:14:46 -0400

    flesch.java: Scanner-using keyboard input reading addedfor simplicity in reading keyboard input; BufferedReader class keyboard input code removed for simplicity in reading keyboard input

--- java/flesch.java
+++ java/flesch.java
@@ -1,61 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class flesch
 {
 	public static void main (String[] args) throws IOException
 	{
-		// To avoid an error, analyze an input file only if one filename
-		// was entered on the command line
-		if ()
+		// Create a Scanner to read the keyboard entered filename of
+		// the file to analyze
+		Scanner keyboardInput = new Scanner (System.in);
+
+		// Receive keyboard input to receive the filename of the file
+		// to analyze
+		String filename = keyboardInput.nextLine();
+
+		// Prepare integer variables to store the analyzed file
+		// values and calculate the reading scores and indexes
+		int syllables = 0;
+		int totalWords = 0;
+		int sentences = 0;
+		int difficultWords = 0;
+
+		// To prepare for the readability index and score
+		// calculations, obtain the analyzed file values
+		obtainFileValues(syllables,
+				 totalWords,
+				 sentences,
+				 difficultWords,
+				 filename);
+
+		// Calculate the reading indexes and score only if words
+		// and syllables were found in the analyzed file to
+		// avoid a division by zero error
+		if ((totalWords > 0) &&
+		    (syllables > 0))
 		{
-			System.out.println("Please enter one filename.");
+			float fleschIndex = calculateFlesch(syllables,
+							    totalWords,
+							    sentences);
+
+			float fleschKincaidIndex = calculateFleschKincaid(syllables,
+									  totalWords,
+									  sentences);
+
+			System.out.println("Flesch Readability Index: " + fleschIndex);
+			System.out.println("Flesch-Kincaid Grade Level Index: " + fleschKincaidIndex);
 		}
 		else
 		{
-			String filename = ;
+			if (!(totalWords > 0))
+				cout << System.out.println("No words found in file.");
+			if (!(sentences > 0))
+				cout << System.out.println("No sentences found in file.");
 
-			// Prepare integer variables to store the analyzed file
-			// values and calculate the reading scores and indexes
-			int syllables = 0;
-			int totalWords = 0;
-			int sentences = 0;
-			int difficultWords = 0;
-
-			// To prepare for the readability index and score
-			// calculations, obtain the analyzed file values
-			obtainFileValues(syllables,
-					 totalWords,
-					 sentences,
-					 difficultWords,
-					 filename);
-
-			// Calculate the reading indexes and score only if words
-			// and syllables were found in the analyzed file to
-			// avoid a division by zero error
-			if ((totalWords > 0) &&
-			    (syllables > 0))
-			{
-				float fleschIndex = calculateFlesch(syllables,
-								    totalWords,
-								    sentences);
-
-				float fleschKincaidIndex = calculateFleschKincaid(syllables,
-										  totalWords,
-										  sentences);
-
-				System.out.println("Flesch Readability Index: " + fleschIndex);
-				System.out.println("Flesch-Kincaid Grade Level Index: " + fleschKincaidIndex);
-			}
-			else
-			{
-				if (!(totalWords > 0))
-					cout << System.out.println("No words found in file.");
-				if (!(sentences > 0))
-					cout << System.out.println("No sentences found in file.");
-
-				System.out.println("As a result, the readability calculations could not be performed.");
-			}
+			System.out.println("As a result, the readability calculations could not be performed.");
 		}
 	}
 }
