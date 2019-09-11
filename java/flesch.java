b8cbe8d6582761e07dc3d956eadb0362ab10132d
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-11 09:00:55 -0400

    flesch.java: No filename on command line error if branch coding starting, code section unfinished

--- java/flesch.java
+++ java/flesch.java
@@ -2,6 +2,11 @@ public class flesch
 {
 	public static void main (String[] args)
 	{
-		;
+		// To avoid an error, analyze an input file only if one filename
+		// was entered on the command line
+		if ()
+		{
+			System.out.println("Please enter one filename.");
+		}
 	}
 }
