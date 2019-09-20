b15ca433b3d0feba3297a238d23ad40418c1af38
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-19 22:11:37 -0400

    Error correction: Line 183: 'word' variable replaced with 'insertionWord' to match function's string parameter name

--- cpp/flesch.cpp
+++ cpp/flesch.cpp
@@ -180,7 +180,7 @@ void insertAlphabetically(vector<string> wordVector,
 	if (wordVector.size() == 0)
 	{
 		// Insert the received word into the Dale-Chall vector
-		wordVector.push_back(word);
+		wordVector.push_back(insertionWord);
 	}
 	else
 	{
