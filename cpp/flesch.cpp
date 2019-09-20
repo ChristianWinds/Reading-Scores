e4a313954dda253416db5b99c02918b6ee3aa9c0
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-19 22:44:42 -0400

    Error correction: main: Use of 'totalSyllables' int variable name in calculateDaleChall call corrected to 'syllables' to match main function's syllable integer variable name

--- cpp/flesch.cpp
+++ cpp/flesch.cpp
@@ -102,7 +102,7 @@ int main(int argc, char* argv[])
 
 		float daleChallScore = calculateDaleChall(difficultWords,
 							  totalWords,
-							  totalSyllables,
+							  syllables,
 							  totalSentences);
 
 		// Print the readability scores
