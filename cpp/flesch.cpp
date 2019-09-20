8628588e097c6584964c089ac001501019225256
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-20 11:04:24 -0400

    insertAlphabetically: Typo in test mode output for vector size at function start corrected

--- cpp/flesch.cpp
+++ cpp/flesch.cpp
@@ -235,7 +235,7 @@ void insertAlphabetically(vector<string> &wordVector,
 
 	if (testMode)
 	{
-		cout << "storeDaleChallList: Dale-Chall vector size at function start == " << wordVector.size() << endl;
+		cout << "insertAlphabetically: Dale-Chall vector size at function start == " << wordVector.size() << endl;
 	}
 
 	if (wordVector.size() == 0)
