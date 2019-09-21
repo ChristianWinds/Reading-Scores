8a8360a72e3a016928ec942727f31c5fd4a46330
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-21 19:02:54 -0400

    calculateFlesch: Precondition and postcondition templates added; calculateFleschKincaid: Precondition, postcondition, alpha calculation, and beta calculation comments added

--- java/Flesch.java
+++ java/Flesch.java
@@ -294,6 +294,9 @@ public class Flesch
 				     int totalWords,
 				     int totalSentences)
 	{
+		// Precondition:
+		// Postcondition:
+
 		float alpha = (float ) totalSyllables / (float) totalWords;
 
 		float beta = (float) totalWords / (float) totalSentences;
@@ -308,11 +311,18 @@ public class Flesch
 					    int totalWords,
 					    int totalSentences)
 	{
-		// Precondition:
-		// Postcondition:
+		// Precondition: The total numbers of syllables, words, and
+		// sentences were obtained, and the total numbers of words and
+		// sentences are not zero
+		// Postcondition: The Flesch-Kincaid Readability Index was
+		// calculated and returned to this function's caller
 
+		// Calculate the alpha value to prepare to calculate the
+		// Flesch-Kincaid Index
 		float alpha = (float) totalSyllables / (float) totalWords;
 
+		// Calculate the beta value to prepare to calculate the
+		// Flesch-Kincaid Index
 		float beta = (float) totalWords / (float) totalSentences;
 
 		float fleschKincaidIndex = alpha * 11.8 + beta * 0.39 - 15.59;
