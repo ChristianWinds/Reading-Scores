0835c5af004bd8c57bed2313cf61eed2c13ff33b
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-22 16:41:48 -0400

    Full primary algorithm transferred from flesch.cpp

--- fortran/flesch.f90
+++ fortran/flesch.f90
@@ -1,3 +1,14 @@
 program flesch
 ! Create an item to hold the Dale-Chall wordlist
+
+! Fill the Dale-Chall vector
+
+! Retrieve the filename for the file to analyze
+
+! Prepare variables for the four values to record from the analyzed file
+
+! Obtain the values need for the readability index and score calculations
+
+! Print the readability scores
+
 end program flesch
