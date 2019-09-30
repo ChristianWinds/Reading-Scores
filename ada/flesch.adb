54e663d300988a88ef583346c50199aef57052a6
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-30 15:49:09 -0400

    Procedure name change

--- ada/flesch.adb
+++ ada/flesch.adb
@@ -2,7 +2,7 @@
 -- https://gcc.gnu.org/onlinedocs/gnat_ugn/Running-a-Simple-Ada-Program.html
 -- Accessed Monday, September 30th, 2019
 with Ada.Text_IO; use Ada.Text_IO;
-procedure Hello is
+procedure flesch is
 begin
    Put_Line ("Hello WORLD!");
-end Hello;
+end flesch;
