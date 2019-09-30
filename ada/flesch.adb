1389bb9307e36c1bcf3de673b12641ddb0ae9f66
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-09-30 15:47:48 -0400

    Hello world print added

---
+++ ada/flesch.adb
@@ -0,0 +1,8 @@
+-- Code from GNAT User's Guide for Native Platforms,
+-- https://gcc.gnu.org/onlinedocs/gnat_ugn/Running-a-Simple-Ada-Program.html
+-- Accessed Monday, September 30th, 2019
+with Ada.Text_IO; use Ada.Text_IO;
+procedure Hello is
+begin
+   Put_Line ("Hello WORLD!");
+end Hello;
