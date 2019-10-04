c1673b3a0091bcaf83671a8d0eed17f376af87a7
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-04 16:36:38 -0400

    Removed code sections

--- ada/flesch.adb
+++ ada/flesch.adb
@@ -3,11 +3,6 @@
 -- Accessed Monday, September 30th, 2019
 with Ada.Command_Line, Ada.Text_IO; use Ada.Command_Line, Ada.Text_IO;
 procedure flesch is
-   Read_From : constant String := "/pub/pounds/CSC330/dalechall/wordlist1995.txt";
-   Write_To  : constant String := "output.txt";
- 
-   Input, Output : File_Type;
-
 -- Code from The Craft of Coding,
 -- https://craftofcoding.wordpress.com/2018/04/04/coding-ada-reading-lines-from-files/
 -- Accessed Tuesday, October 1st, 2019
@@ -15,34 +10,7 @@ procedure flesch is
    sline : unbounded_string;
    fname : unbounded_string;
    nameOK : boolean := false;
--- Code from Rosetta Code,
--- https://www.rosettacode.org/wiki/File_input/output#Ada
--- Accessed Monday, September 30th, 2019
 begin
-   begin
-      Open (File => Input,
-            Mode => In_File,
-            Name => Read_From);
-   exception
-      when others =>
-         Put_Line (Standard_Error,
-                   "Can not open the file '" & Read_From & "'. Does it exist?");
-         Set_Exit_Status (Failure);
-         return;
-   end;
- 
-   begin
-      Create (File => Output,
-              Mode => Out_File,
-              Name => Write_To);
-   exception
-      when others =>
-         Put_Line (Standard_Error,
-                   "Can not create a file named '" & Write_To & "'.");
-         Set_Exit_Status (Failure);
-         return;
-   end;
-
 -- Code from The Craft of Coding,
 -- https://craftofcoding.wordpress.com/2018/04/04/coding-ada-reading-lines-from-files/
 -- Accessed Tuesday, October 1st, 2019
@@ -61,24 +29,4 @@ loop
    get_line(infp,sline);
    -- do something with the line of text
    end loop;
-   
- 
-   loop
-      declare
-         Line : String := Get_Line (Input);
-      begin
-         -- You can process the contents of Line here.
-         Put_Line (Output, Line);
-      end;
-   end loop;
-   Close (Input);
-   Close (Output);
-exception
-   when End_Error =>
-      if Is_Open(Input) then 
-         Close (Input);
-      end if;
-      if Is_Open(Output) then 
-         Close (Output);
-      end if;
 end flesch;
