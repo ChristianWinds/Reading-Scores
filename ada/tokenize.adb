50011df16f7f31dcc58aecf6c4796824b6318a91
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-04 18:50:26 -0400

    Added string tokenizer code

---
+++ ada/tokenize.adb
@@ -0,0 +1,20 @@
+-- Code from Rosetta Code,
+-- https://www.rosettacode.org/wiki/Tokenize_a_string#Ada
+-- Accessed October 4, 2019
+with Ada.Text_IO, Ada.Containers.Indefinite_Vectors;
+use  Ada.Text_IO, Ada.Containers;
+ 
+procedure tokenize is
+  package String_Vector is new Indefinite_Vectors (Natural,String); use String_Vector;
+  s       : String   := "Hello,How,Are,You,Today" & ",";
+  current : Positive := s'First;      
+  v       : Vector;
+begin
+  for i in s'range loop 
+    if s (i) = ',' or i = s'last then 
+      v.append (s (current .. i-1));
+      current := i + 1;
+    end if;
+   end loop;
+  for s of v loop put(s & "."); end loop;
+end tokenize;
