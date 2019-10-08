db87b56cffde2aa21e098b3a341ef76a8372c4eb
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-08 11:26:30 -0400

    Updated

--- ada/flesch.adb
+++ ada/flesch.adb
@@ -4,9 +4,12 @@
 with Ada.Text_IO;  use Ada.Text_IO;
 with Line_By_Line_Pack;
 use Line_By_Line_Pack;
+with pounds_tokenize_pack;
+use pounds_tokenize_pack;
  
-procedure Test_Run is
+procedure flesch is
 
 begin
    Line_By_Line;
-end Test_Run;
+   toarray;
+end flesch;
