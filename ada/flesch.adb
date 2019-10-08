7c49211ba8da5ccd52e8dda7703f74527a8eb327
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-08 10:51:15 -0400

    Procedure Use Attempt

--- ada/flesch.adb
+++ ada/flesch.adb
@@ -2,16 +2,11 @@
 -- https://www.rosettacode.org/wiki/Read_a_file_line_by_line#Ada
 -- Accessed October 7, 2019
 with Ada.Text_IO;  use Ada.Text_IO;
+with Line_By_Line_Pack;
+use Line_By_Line_Pack;
  
-procedure Line_By_Line is
-   File : File_Type;
+procedure Test_Run is
+
 begin
-   Open (File => File,
-         Mode => In_File,
-         Name => "twoSentences.txt");
-   While not  End_Of_File (File) Loop
-      Put_Line (Get_Line (File));
-   end loop;
- 
-   Close (File);
-end Line_By_Line;
+   Line_By_Line;
+end Test_Run;
