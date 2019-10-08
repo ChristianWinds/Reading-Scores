245d34f36e01a447b4e3f59892dce5b55447bfaa
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-08 11:55:06 -0400

    Line_By_Line use removed

--- ada/flesch.adb
+++ ada/flesch.adb
@@ -1,15 +1,9 @@
--- Code from Rosetta Code,
--- https://www.rosettacode.org/wiki/Read_a_file_line_by_line#Ada
--- Accessed October 7, 2019
 with Ada.Text_IO;  use Ada.Text_IO;
-with Line_By_Line_Pack;
-use Line_By_Line_Pack;
 with pounds_tokenize_pack;
 use pounds_tokenize_pack;
  
 procedure flesch is
 
 begin
---   Line_By_Line;
    toarray;
 end flesch;
