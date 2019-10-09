6b160c869c4b09220edc6e7a0221de7a38faa757
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-09 18:12:18 -0400

    Header added

--- ada/pounds_tokenize_pack.ads
+++ ada/pounds_tokenize_pack.ads
@@ -1,3 +1,7 @@
+-- Programmer Name: Christian Winds
+-- Course: CSC330 Organization of Programming Languages
+-- School Year: 2019-2020 School Year
+
 package pounds_tokenize_pack is
 	procedure toarray (words: out Integer);
 end pounds_tokenize_pack;
