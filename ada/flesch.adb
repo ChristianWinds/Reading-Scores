7f33fa8cb337b6a0e8d65e63455d73da9f36439c
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-09 18:04:36 -0400

    Header added

--- ada/flesch.adb
+++ ada/flesch.adb
@@ -1,3 +1,7 @@
+-- Programmer Name: Christian Winds
+-- Course: CSC330 Organization of Programming Languages
+-- School Year: 2019-2020 School Year
+
 with Ada.Text_IO;  use Ada.Text_IO;
 with pounds_tokenize_pack;
 use pounds_tokenize_pack;
