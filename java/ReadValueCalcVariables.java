dc105e193b89b2bda7db32971dd81b6310bda4b9
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-09 17:56:05 -0400

    Header; comment added

--- java/ReadValueCalcVariables.java
+++ java/ReadValueCalcVariables.java
@@ -1,5 +1,11 @@
+/* Programmer Name: Christian Winds
+ * Course: CSC330 Organization of Programming Languages
+ * School Year: 2019-2020 School Year */
+
 public class ReadValueCalcVariables
 {
+	// Create variables to hold the values necessary for the readability
+	// score calculations
 	int syllables = 0;
 	int totalWords = 0;
 	int sentences = 0;
