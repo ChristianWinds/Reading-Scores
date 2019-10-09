f036b04f546dd413e3d7ca5424720290e1556e08
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-09 17:57:07 -0400

    Header, comment added

--- java/WordVector.java
+++ java/WordVector.java
@@ -1,5 +1,10 @@
+/* Programmer Name: Christian Winds
+ * Course: CSC330 Organization of Programming Languages
+ * School Year: 2019-2020 School Year */
+
 import java.util.*;
 public class WordVector
 {
+	// Create a vector to hold a collection of words
 	Vector<String> wordVector = new Vector();
 }
