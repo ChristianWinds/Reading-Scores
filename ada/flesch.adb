8398ca8fb6e54efbb0be679ca758b4c44044380b
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-09 22:41:26 -0400

    Input

--- ada/flesch.adb
+++ ada/flesch.adb
@@ -2,6 +2,7 @@
 -- Course: CSC330 Organization of Programming Languages
 -- School Year: 2019-2020 School Year
 
+with Ada.Command_Line;
 with Ada.Text_IO;  use Ada.Text_IO;
 with pounds_tokenize_pack;
 use pounds_tokenize_pack;
@@ -11,5 +12,5 @@ procedure flesch is
    syllables	: Integer := 0;
    sentences	: Integer := 0; 
 begin
-   toarray(total_words, syllables, sentences);
+   toarray(total_words, syllables, sentences, Ada.Command_Line.Argument(1));
 end flesch;
